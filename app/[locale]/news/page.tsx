e8f54838f8549a49d3b372a20fe8291bfd264450
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2024-04-09 19:28:34 +0900

    build success 240409 working on news page

--- app/[locale]/news/page.tsx
+++ app/[locale]/news/page.tsx
@@ -1,6 +1,51 @@
+'use client';
 import styles from './styles.module.css'
-export default async function Page() {
+import {useEffect, useState} from "react";
+import image0 from '@/public/news/retriseminar.jpeg';
+import image1 from '@/public/news/herald.jpeg';
+import image2 from '@/public/news/herald2.jpeg';
+import image3 from '@/public/news/herald3.jpeg';
+
+import Image from 'next/image';
+export default function Page() {
+
+    const arr = Array(4).fill(0);
+    const imgSrc = [image0, image1, image2, image3];
+
+    const [currentIdx, setCurrentIdx] = useState(-1);
+    const [isHover, setIsHover] = useState(false);
+    const handleClick = (idx:any) => {
+        setCurrentIdx(idx);
+        setIsHover(true);
+    }
+
+    useEffect(() => {
+        console.log(currentIdx)
+    }, [currentIdx])
+
     return (
-        <div className={styles.newsWrapper}>News Page</div>
+        <div className={styles.newsWrapper}>
+            <div className={styles.newsContent}>
+                {
+                    arr.map((a, i) => {
+                        return (
+                            <div
+                                onClick={() => {handleClick(i)}} key={i}
+                                className={`${currentIdx !== i && isHover ? styles.hide : ''}`}
+                            >
+                                <Image src={imgSrc[i]} fill={true} style={{objectFit: 'cover'}} alt={`news image ${i}`}/>
+                                {`news${i}`}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div onClick={() => setIsHover(false)}className={styles.newsContent}>
+                <div>news5</div>
+                <div>news6</div>
+                <div>news7</div>
+                <div>news8</div>
+            </div>
+        </div>
     )
 }
--- components/Partners/index.tsx
+++ components/Partners/index.tsx
@@ -1,6 +1,6 @@
 import styles from './styles.module.css';
 import Image from 'next/image';
-import lbankLogo from '@/public/lbank-logo.png';
+import lbankLogo from '@/public/lbank-logo-yellow.webp';
 import cointrLogo from '@/public/cointrwhite-logo.png';
 import chartBg from '@/public/chart-bg-white.png';
 import vobLogo from '@/public/vob_logo_2.png';
@@ -11,18 +11,47 @@ export function Partners() {
     return (
         <div>
         <div className={styles.title}>Get Started with VOB</div>
+        <div className={styles.desc}>Start trading now with VOB&apos;s partner and globally verified CEX</div>
         <div className={styles.wrapper}>
-            <div className={`${styles.excElem}`}>
+            <div className={`${styles.excElem} ${styles.excElem1}`}>
                 <div className={styles.vob}><LogoPlain/></div>
-
                 <div className={styles.startElem}>
                     <Link href="https://www.lbank.com/trade/vob_usdt" target="_blank" rel="noopener noreferrer" locale={false} >
                     <button className={styles.startBtn}><span>Get Started</span></button>
                     </Link>
                 </div>
 
-                <div className={styles.exchange}><Image src={lbankLogo} width={200} height={100} alt="l bank logo"></Image></div>
+                <div className={styles.exchange}><Image className="p-4" src={lbankLogo} width={200} height={100} alt="l bank logo"></Image></div>
                 <div className={styles.crossElem}><Image src={crossWhite} width={25} height={25} alt="white cross"/></div>
+                {/*<div className={styles.circleElem}>*/}
+                {/*    <svg*/}
+                {/*        version="1.1"*/}
+                {/*        xmlns="http://www.w3.org/2000/svg"*/}
+                {/*        viewBox="0 0 102 102"*/}
+                {/*        className={styles.svgContainer}*/}
+                {/*    >*/}
+                {/*        <circle*/}
+                {/*            className={styles.btnCircle}*/}
+                {/*            cx="51"*/}
+                {/*            cy="51"*/}
+                {/*            r="50"*/}
+                {/*            fill="none"*/}
+                {/*            stroke="#fff"*/}
+                {/*            strokeWidth="0"*/}
+                {/*        ></circle>*/}
+                {/*        <circle*/}
+                {/*            className={styles.btnCircleProg}*/}
+                {/*            cx="51"*/}
+                {/*            cy="51"*/}
+                {/*            r="50"*/}
+                {/*            fill="none"*/}
+                {/*            stroke="#FFD800"*/}
+                {/*            strokeWidth="1"*/}
+                {/*            strokeDasharray="0 351"*/}
+                {/*            strokeDashoffset="0"*/}
+                {/*        ></circle>*/}
+                {/*    </svg>*/}
+                {/*</div>*/}
                 <div className={styles.circleElem}>
                     <svg
                         version="1.1"
@@ -30,37 +59,43 @@ export function Partners() {
                         viewBox="0 0 102 102"
                         className={styles.svgContainer}
                     >
-                        <circle
+                        <rect
                             className={styles.btnCircle}
-                            cx="51"
-                            cy="51"
-                            r="50"
+                            x="0"
+                            y="0"
+                            width="100"
+                            height="100"
                             fill="none"
-                            stroke="#bbb"
-                            strokeWidth="1"
-                        ></circle>
-                        <circle
+                            stroke="#fff"
+                            strokeWidth="0"
+                            rx="10" // Border radius for the rectangle's x-axis
+                            ry="10" // Border radius for the rectangle's y-axis
+                        ></rect>
+                        <rect
                             className={styles.btnCircleProg}
-                            cx="51"
-                            cy="51"
-                            r="50"
+                            x="0.5"
+                            y="0.5"
+                            width="101"
+                            height="101"
                             fill="none"
-                            stroke="#00FCFF"
+                            stroke="#FFD800"
                             strokeWidth="1"
-                            strokeDasharray="0 351"
+                            strokeDasharray="0 400"
                             strokeDashoffset="0"
-                        ></circle>
+                            rx="10" // Border radius for the rectangle's x-axis
+                            ry="10" // Border radius for the rectangle's y-axis
+                        ></rect>
                     </svg>
                 </div>
             </div>
-            <div className={`${styles.excElem}`}>
+            <div className={`${styles.excElem} ${styles.excElem2}`}>
                 <div className={styles.vob}><LogoPlain/></div>
                 <div className={styles.startElem}>
                     <Link href="https://www.cointr.pro/en-us/spot/VOBUSDT" target="_blank" rel="noopener noreferrer" locale={false}>
                     <button className={styles.startBtn}><span>Get Started</span></button>
                     </Link>
                 </div>
-                <div className={styles.exchange}><Image className="pt-2" src={cointrLogo} width={200} height={100} alt="coin t r logo"></Image></div>
+                <div className={styles.exchange}><Image className="p-2" src={cointrLogo} width={200} height={100} alt="coin t r logo"></Image></div>
                 <div className={styles.crossElem}><Image src={crossWhite} width={25} height={25} alt="white cross"/></div>
                 <div className={styles.circleElem}>
                     <svg
@@ -69,26 +104,32 @@ export function Partners() {
                         viewBox="0 0 102 102"
                         className={styles.svgContainer}
                     >
-                        <circle
+                        <rect
                             className={styles.btnCircle}
-                            cx="51"
-                            cy="51"
-                            r="50"
+                            x="0"
+                            y="0"
+                            width="100"
+                            height="100"
                             fill="none"
-                            stroke="#bbb"
-                            strokeWidth="1"
-                        ></circle>
-                        <circle
+                            stroke="#fff"
+                            strokeWidth="0"
+                            rx="10" // Border radius for the rectangle's x-axis
+                            ry="10" // Border radius for the rectangle's y-axis
+                        ></rect>
+                        <rect
                             className={styles.btnCircleProg}
-                            cx="51"
-                            cy="51"
-                            r="50"
+                            x="0.5"
+                            y="0.5"
+                            width="101"
+                            height="101"
                             fill="none"
-                            stroke="#00FCFF"
+                            stroke="#03D56F"
                             strokeWidth="1"
-                            strokeDasharray="0 350"
+                            strokeDasharray="0 400"
                             strokeDashoffset="0"
-                        ></circle>
+                            rx="10" // Border radius for the rectangle's x-axis
+                            ry="10" // Border radius for the rectangle's y-axis
+                        ></rect>
                     </svg>
                 </div>
             </div>
