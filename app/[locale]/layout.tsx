06278321a42d0d6d0f412bba6b5d9cf4aa19eaa1
jinwoo <jinw0909@gmail.com>
2024-07-04 14:35:33 +0900

    240704 add icon.ico

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout ({
   return (
       <html lang={locale}>
           <head>
-            {/* <link rel="icon" href="/favicon.ico" type="image/x-icon"/> */}
+            <link rel="icon" href="/icon.ico" type="image/x-icon" sizes="any"/>
             <title>VOB | VISION OF BLOCKCHAIN</title>
             <meta name="description" content={t('main.description')}/>
           </head>
