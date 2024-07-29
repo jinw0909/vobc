714dd98061848de4dd5988ab0f004c8595825ec4
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2024-07-29 11:11:12 +0900

    240729 favicon.ico

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
         //     priority: 1,
         // },
         {
-            url: 'https://www.vobc.io/favicons/favicon-96x96.png',
+            url: 'https://www.vobc.io/favicon.io',
             lastModified,
             changeFrequency: 'yearly',
             priority: 1,
