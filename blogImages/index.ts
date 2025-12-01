d0c4aaeb728c77c049fcfd085e13736516b6f5aa
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2025-12-01 14:41:08 +0900

    style adustment

--- blogImages/index.ts
+++ blogImages/index.ts
@@ -1,10 +1,10 @@
 import image from 'next/image';
-import blogImage1 from '@/public/blog/thumbnail_0.jpg'
+import blogImage1 from '@/public/blog/thumbnail_0.png'
 import blogImage2 from '@/public/blog/thumbnail_1.jpg'
 
 const blogImages: any = [
-    { image: blogImage1, id: 1, url: '/blog/thumbnail_0.jpg'},
-    { image: blogImage1, id: 2, url: '/blog/thumbnail_0.jpg'},
+    { image: blogImage1, id: 1, url: '/blog/thumbnail_0.png'},
+    { image: blogImage1, id: 2, url: '/blog/thumbnail_0.png'},
     // { image: blogImage1, id: 3, url: '/blog/thumbnail_0.jpg'},
     // { image: blogImage1, id: 4, url: '/blog/thumbnail_0.jpg'},
 ]
